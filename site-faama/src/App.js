f0e80968749cd6246d919c3c69809f54107b4f9c
apolo-matos <apollomatos22@gmail.com>
2026-02-09 16:59:28 -0300

    Ajustes de UI na seção de Cursos e organizacao de componentes

--- site-faama/src/App.js
+++ site-faama/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Inicio from './pages/Inicio';
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Curso from "./components/Curso";
 import './App.css';
 
 
 function App() {
   return (
     <div className="FAAMA">
-      <Inicio />
+      <Navbar />
+      <Banner />
+      <Curso />
+   
     </div>
   );
 }
--- site-faama/src/components/Curso.js
+++ site-faama/src/components/Curso.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import './Curso.css';
+import '../styles/Curso.css';
+import img1 from '../Assets/Group 53.png';
+import img2 from '../Assets/Group 56.png';
+import img3 from '../Assets/Group 55.png';
+import img4 from '../Assets/Group 57.png';
 
 const Curso = () => {
   const listaCursos = [
-    { id: 1, nome: "DIREITO", img: "4Group 53.png", pos: "baixo" },
-    { id: 2, nome: "PEDAGOGIA", img: "Group 56.png", pos: "cima" },
-    { id: 3, nome: "A.D.S.", img: "Group 55.png", pos: "baixo" },
-    { id: 4, nome: "PSICOLOGIA", img: "Group 57.png", pos: "cima" },
-    { id: 5, nome: "TEOLOGIA", img: "Group 54.png", pos: "baixo" },
+    { id: 1, nome: "DIREI TO", img: img1, pos: "baixo" },
+    { id: 2, nome: "PEDA GOGIA", img: img2, pos: "cima" },
+    { id: 3, nome: "A.D.S.", img: img3, pos: "baixo" },
+    { id: 4, nome: "PSICO LOGIA", img: img4, pos: "cima" },
+ 
   ];
 
   return (
     <section className="container-cursos">
-      {listaCursos.map(curso => (
-        <i 
+      <h2 className="curso-secao">Cursos</h2>
+      {listaCursos.map((curso) => (
+        <div 
           key={curso.id} 
           className={`card-curso ${curso.pos}`}
           style={{ backgroundImage: `url(${curso.img})` }}
         >
           <strong>{curso.nome}</strong>
-        </i>
+        </div>
       ))}
     </section>
   );
